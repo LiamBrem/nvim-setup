2f6373a1e574b8e6864a9ebc0be5d86ef1a3d5fd
liambrem <liam.e.brem@gmail.com>
2026-04-02 11:05:25 -0400

    tresitter context plugin

--- lua/liambrem/plugins/treesitter.lua
+++ lua/liambrem/plugins/treesitter.lua
@@ -1,23 +1,36 @@
 -- treesitter
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- A list of parser names, or "all"
-			ensure_installed = { "c", "lua", "markdown", "markdown_inline", "cpp", "python", "rust", "java" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				-- A list of parser names, or "all"
+				ensure_installed = { "c", "lua", "markdown", "markdown_inline", "cpp", "python", "rust", "java" },
 
-			sync_install = false,
-			auto_install = true,
-			ignore_install = { "javascript" },
+				sync_install = false,
+				auto_install = true,
+				ignore_install = { "javascript" },
 
-			highlight = {
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+				indent = {
+					enable = false,
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup({
 				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = {
-				enable = false,
-			},
-		})
-	end,
+				max_lines = 3,
+				multiline_threshold = 1,
+				mode = "cursor",
+			})
+		end,
+	},
 }
